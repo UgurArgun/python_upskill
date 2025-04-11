bea0d138a00e8ac288b495c4ff9e8cb04176e30c
ugur <ugur@skenariolabs.com>
2025-04-12 02:53:01 +0300

    python programming

---
+++ day1/first_python_program.py
@@ -0,0 +1,34 @@
+print('Python Programming')
+
+# This is a comment
+
+"""
+This is a multi-line comment
+"""
+
+name_of_student = "Elena"
+print(name_of_student)
+print(type(name_of_student))
+
+name_of_student = 3.5
+print(name_of_student)
+print(type(name_of_student))
+
+
+name_of_student= True
+print(name_of_student)
+print(type(name_of_student))
+
+s ='35'
+
+num = int(s)
+print(num)
+print(type(num))
+
+print("------------------------------------------------")
+
+prog_language = "Python"
+version = 3.13
+
+print("My programming language is {} and version is {}".format(prog_language, version))
+print(f"My programming language is {prog_language} and version is {version}")
